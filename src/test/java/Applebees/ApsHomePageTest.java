cfc22e1af76095ca0859f2ddcd309b756b1279d7
sathish <sathishmcamku@gmail.com>
2021-07-20 22:43:46 +0530

    test

--- src/test/java/Applebees/ApsHomePageTest.java
+++ src/test/java/Applebees/ApsHomePageTest.java
@@ -71,7 +71,7 @@ public class ApsHomePageTest extends Base{
 		lpo.Emailid().sendKeys(data.get(1));
 		lpo.Pwd().sendKeys(data.get(2));
 		
-		System.out.println("Valid Credentials");
+		System.out.println("Valid Credentials given");
 		lpo.Login().click();
 		lpo.MenuClickClick().click();
 		lpo.Locsubnav().click();
